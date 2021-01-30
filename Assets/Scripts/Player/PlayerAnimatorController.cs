3d2ca93dea2fb9c02bddc25854e36fbf607a675a
Jakub Mirovský <mirovsky.jakub@gmail.com>
2021-01-30 22:25:41 +0100

    add robot animations

---
+++ Assets/Scripts/Player/PlayerAnimatorController.cs
@@ -0,0 +1,18 @@
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class PlayerAnimatorController : MonoBehaviour
+{
+    [SerializeField]
+    AnimatorController animatorController;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
